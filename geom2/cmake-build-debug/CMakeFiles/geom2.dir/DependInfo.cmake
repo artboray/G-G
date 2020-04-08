d643c50f9e0c26f22d9add33a369af0bfb566ab8
Artem Boray <art.war@mail.ru>
2020-04-08 15:04:29 +0300

    FF

--- geom2/cmake-build-debug/CMakeFiles/geom2.dir/DependInfo.cmake
+++ geom2/cmake-build-debug/CMakeFiles/geom2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/borat/CLionProjects/tmp/geom2/main.cpp" "/Users/borat/CLionProjects/tmp/geom2/cmake-build-debug/CMakeFiles/geom2.dir/main.cpp.o"
+  "/Users/borat/CLionProjects/FORGIT/geom2/main.cpp" "/Users/borat/CLionProjects/FORGIT/geom2/cmake-build-debug/CMakeFiles/geom2.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- geom2/cmake-build-debug/cmake_install.cmake
+++ geom2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/borat/CLionProjects/tmp/geom2
+# Install script for directory: /Users/borat/CLionProjects/FORGIT/geom2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/borat/CLionProjects/tmp/geom2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/borat/CLionProjects/FORGIT/geom2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
